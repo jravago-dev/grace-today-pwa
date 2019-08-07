6e22c164b3d443747a1a41a12b0a163d2feaa9d0
Justin R <justinshobby@zoho.com>
2019-08-08 07:15:10 +0800

    Juts (0808) - added express and path depencies to the project

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/*', function(req, res) {
 });
 
 // default Heroku PORT
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Express server listening on port', port)
 });
