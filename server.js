4ff7a447da14fa3d0722c190c767a3788a06f906
Justin R <justinshobby@zoho.com>
2019-08-08 02:01:44 +0800

    Juts (0808) - added console log on port

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ app.get('/*', function(req, res) {
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Express server listening on port', port)
+});
