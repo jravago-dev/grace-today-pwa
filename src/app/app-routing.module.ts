a668708f178c49c7cd921c9435b6015027fac9f9
Justin R <justinshobby@zoho.com>
2019-08-08 10:41:28 +0800

    Juts (0808) - added credits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { VerseDashboardComponent } from './components/verse-dashboard/verse-dashboard.component';
+import { CreditsComponent } from './components/general/credits/credits.component';
 
 
 const routes: Routes = [ 
@@ -10,6 +11,9 @@ const routes: Routes = [
   },
   {
     path: 'about', component: AboutComponent
+  },
+  {
+    path: 'thanks', component: CreditsComponent
   },
   {
     path: '',
