bccba77daabb679ec3009383289a957938c10bea
Justin R <justinshobby@zoho.com>
2019-08-21 14:12:18 +0800

    Juts (0821) - Added initial dashboard contents when user is logged in.

--- src/app/components/user-dashboard/user-dashboard.component.ts
+++ src/app/components/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BibleApiService } from 'src/app/services/bible-api.service';
+import { take } from 'rxjs/operators'
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  votd;
+  liturgyData;
+
+  constructor(private bibleService: BibleApiService, private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.bibleService.getVerseToday()
+    .pipe(
+      take(1),
+    ).subscribe(r=>{
+      this.votd = r.verse.details 
+    })  
+    
+    this.bibleService.getReadingsToday()
+    .pipe(
+      take(1),
+    ).subscribe(r=>{
+      this.liturgyData = r.data
+   
+    })       
+
+  }
+
+  returnQuoted(text){
+    return `"${text}"`;
+  } 
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 5000,
+    });
   }
 
 }
